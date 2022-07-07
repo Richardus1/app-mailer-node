f681fad1d5d208fa8d8cb980f1bc4c5efaf82c8b
Richardus <rikkodigital@gmail.comm>
2022-07-07 17:06:58 -0500

    Error de token

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,8 +4,8 @@ const { google } = require("googleapis");
 const router = express.Router();
 require("dotenv").config();
 
-router.post("/sendEmail", (req, res)=> {
-  const {name, email, phone, message} = req.body;
+router.post("/sendEmail", (req, res) => {
+  const { name, email, phone, message } = req.body;
   const contentHtml = `
   <h1>Formulario de Nodemailer</h1>
   <ul>
@@ -17,52 +17,61 @@ router.post("/sendEmail", (req, res)=> {
   `;
 
   const CLIENT_ID = process.env.CLIENT_ID;
-  const CLIENT_SECRET =  process.env.CLIENT_SECRET;
+  const CLIENT_SECRET = process.env.CLIENT_SECRET;
   const REDIRECT_URI = process.env.REDIRECT_URI;
   const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
-  
+  const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+  );
 
-  const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-
-  oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+  oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
   async function sendMail() {
-    try {  
-      const accessToken = await oAuth2Client.getAccessToken();
+    try {
+      const accessToken = await oAuth2Client.getAccessToken(Error);
+      if (Error)
+        return console.log(
+          "Fallo al conseguir el refreshToken, el correo no se envió"
+        );
+
       const transporter = nodemailer.createTransport({
         service: "gmail",
-        auth:{
+        auth: {
           type: "OAuth2",
           user: process.env.USER,
           clientId: CLIENT_ID,
           clientSecret: CLIENT_SECRET,
           refreshToken: REFRESH_TOKEN,
-          accessToken: accessToken
-        }        
+          accessToken: accessToken,
+        },
       });
 
       const mailOptions = {
         from: "La página!!! <rikkodigital@gmail.com>",
         to: process.env.DESTINO,
         subject: "Prueba de app-mailer-node",
-        html: contentHtml
+        html: contentHtml,
       };
-      
 
       const result = await transporter.sendMail(mailOptions);
       return result;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
   sendMail()
-  .then((result) => res.status(200).send(`<h1>Enviado con éxito!</h1><form action="/" method=GET>
+    .then((result) =>
+      res.status(200)
+        .send(`<h1>Enviado con éxito!</h1><form action="/" method=GET>
   <button class="btn btn-primary btn-black">Ir al inicio></button>
   </form>  
-  `))
-  .catch((error) => console.log(error.message))
+  `)
+    )
+    .catch((error) => console.log(error.message));
   //res.send("Enviado")
-}); 
+});
 
-module.exports = router
+module.exports = router;
